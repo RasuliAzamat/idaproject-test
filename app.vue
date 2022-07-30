922111415e64ce1f2e63560718b769fa73b15365
Habibullo <fayzzodah@gmail.com>
2022-07-30 05:29:32 +0500

    Make responsive markup

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="app-container flex-container">
         <aside class="aside">
             <h2>Добавление товара</h2>
 
@@ -31,6 +31,9 @@
                         class="form__input"
                         :error="false"
                     />
+                    <small v-if="false" class="form__error">
+                        Далеко-далеко за словесными горами в стране.
+                    </small>
                 </div>
 
                 <div class="form__field _required">
@@ -95,42 +98,42 @@ const products = ref([
     {
         id: 1,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 1',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 10000,
     },
     {
         id: 2,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 2',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 12000,
     },
     {
         id: 3,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 3',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 6000,
     },
     {
         id: 4,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 4',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 20000,
     },
     {
         id: 5,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 5',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 2000,
     },
     {
         id: 6,
         img: 'https://www.lifeinnorway.net/wp-content/uploads/2022/02/faroe-islands-waterfall-sunset.jpg',
-        title: 'Наименование товара',
+        title: 'Наименование товара 6',
         text: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
         price: 5500,
     },
@@ -168,17 +171,20 @@ const options = ref([
     font-family: 'Source Sans Pro', sans-serif;
 }
 
-.container {
+.app-container {
     max-width: 1440px;
     margin: 0 auto;
     padding: 5px 10px;
+}
 
+.flex-container {
     display: flex;
+    flex-direction: column;
 }
 
 .aside {
     flex-basis: 25%;
-    margin-right: 20px;
+    margin-bottom: 70px;
 
     h2 {
         margin: 15px 0 30px;
@@ -186,7 +192,7 @@ const options = ref([
 }
 
 .form {
-    padding: 25px;
+    padding: 15px;
     border-radius: 4px;
 
     box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04),
@@ -242,10 +248,58 @@ const options = ref([
 
 .products {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr;
     grid-template-rows: max-content;
     gap: 20px 20px;
 
     margin-bottom: 50px;
+}
+
+
+@media (min-width: 600px) {
+    .flex-container {
+        flex-direction: row;
+    }
+
+    .form {
+        padding: 25px;
+    }
+
+    .aside {
+        flex-basis: 45%;
+    }
+
+    .main {
+        flex-basis: 55%;
+    }
+}
+
+@media (min-width: 768px) {
+    .aside {
+        flex-basis: 35%;
+        margin-bottom: 0;
+        margin-right: 20px;
+    }
+
+    .main {
+        flex-basis: 65%;
+    }
+    .products {
+        grid-template-columns: 1fr 1fr;
+    }
+}
+
+@media (min-width: 1024px) {
+    .aside {
+        flex-basis: 25%;
+    }
+
+    .main {
+        flex-basis: 75%;
+    }
+
+    .products {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
 }
 </style>
--- components/AppProduct.vue
+++ components/AppProduct.vue
@@ -80,29 +80,43 @@ const {img, title, text, price} = defineProps({
 
     &--trashbox {
         position: absolute;
-        top: -15px;
-        right: -15px;
+        top: 0px;
+        right: 0px;
 
         padding: 9px;
-        border-radius: 10px;
-
-        cursor: pointer;
-        transform: scale(0);
-        opacity: 0;
+        border-bottom-left-radius: 10px;
+        border-top-right-radius: 4px;
 
         background-color: #ff8484;
         box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-        transition: all 0.3s ease-in-out;
+        transition: all 0.2s ease-in-out;
     }
 
     &:hover {
         box-shadow: 0px 12px 14px rgba(0, 0, 0, 0.1);
+    }
+}
 
-        .products__item--trashbox {
-            top: -10px;
-            right: -10px;
-            transform: scale(1);
-            opacity: 1;
+@media (min-width: 1024px) {
+    .products__item {
+        &--trashbox {
+            top: -15px;
+            right: -15px;
+
+            border-radius: 10px;
+
+            cursor: pointer;
+            transform: scale(0);
+            opacity: 0;
+        }
+
+        &:hover {
+            .products__item--trashbox {
+                top: -10px;
+                right: -10px;
+                transform: scale(1);
+                opacity: 1;
+            }
         }
     }
 }
